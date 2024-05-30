d4859e1aa534186b22e8cdf7675e4da3d9f2951c
Jonathon Brady <jonathonabrady@gmail.com>
2024-05-29 22:34:29 -0700

    remove dependency installation

--- build.py
+++ build.py
@@ -38,20 +38,10 @@ for decomp_root, _, files in os.walk(DECOMP_ROOT):
             shutil.copy2(p2gz_file, decomp_file)
             print(f'Replaced {decomp_file} with {p2gz_file}')
 
-if not os.path.exists(os.path.join(os.getcwd(), 'ninja.exe')):
-    ninja_zip = requests.get('https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-win.zip').content
-    with zipfile.ZipFile(BytesIO(ninja_zip)) as f:
-        f.extractall()
-
 subprocess.run('python configure.py --no-check', cwd='pikmin2')
 subprocess.run('ninja', cwd='pikmin2')
 
-if not os.path.exists(os.path.join(os.getcwd(), 'nodtool.exe')):
-    nodtool = requests.get('https://github.com/AxioDL/nod/releases/download/v1.0/nodtool.v1.win64.exe')
-    with open('nodtool.exe', 'wb') as f:
-        f.write(nodtool.content)
-
 if not os.path.exists(os.path.join(os.getcwd(), 'root')):
     subprocess.run(f'nodtool extract "{iso}" root')
-    
+
 shutil.copy2('pikmin2/build/pikmin2.usa/main.dol', 'root/sys/main.dol')
