daeb439022aa6a50c3b0c36e4fc49c3758b3b53e
Jonathon Brady <jonathonabrady@gmail.com>
2025-10-28 15:24:18 -0700

    remove unused members

--- include/p2gz/PokoEditor.h
+++ include/p2gz/PokoEditor.h
@@ -7,26 +7,14 @@
 namespace gz {
 struct PokoEditor {
 public:
-	PokoEditor() { percent_cutscene = false; }
+	PokoEditor() { }
 	~PokoEditor() { }
 
 	void init();
 	void update();
 
-	bool check() { return percent_cutscene; }
-
-	bool clear()
-	{
-		if (percent_cutscene) {
-			percent_cutscene = false;
-			return true;
-		}
-		return false;
-	}
-
 private:
 	gz::DecimalInputOption* pokos;
-	bool percent_cutscene;
 };
 } // namespace gz
 #endif
