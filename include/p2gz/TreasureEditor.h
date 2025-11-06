31c534039fe986f3ae3e718c41b1d203d78c0670
Jonathon Brady <jonathonabrady@gmail.com>
2025-11-06 12:24:46 -0800

    actually check which functions need to be public instead of just guessing

--- include/p2gz/TreasureEditor.h
+++ include/p2gz/TreasureEditor.h
@@ -13,21 +13,21 @@ public:
 	void disable() { enabled = false; }
 	Game::Pellet* get_active_treasure() { return active_treasure; }
 	bool is_enabled() { return enabled; }
+	void reset_active_treasure() { active_treasure->setPosition(initial_position, false); }
 
 	void add(Game::Pellet*);
+	void clear_treasures();
 	void enable();
 	void init();
 	void set_collected(Game::Pellet*, bool);
+	void snap_to_nearest_waypoint();
 
 private:
-	void reset_active_treasure() { active_treasure->setPosition(initial_position, false); }
 	void toggle_collected(bool) { }
 
-	void clear_treasures();
 	void find_treasure();
 	void handle_breadbug(Game::EnemyBase*);
 	void handle_dweevil(Game::EnemyBase*);
-	void snap_to_nearest_waypoint();
 
 	ListMenu* treasures;
 	Game::Pellet* active_treasure;
