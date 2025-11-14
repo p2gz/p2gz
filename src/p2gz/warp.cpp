08a97600af8e18a8b87fb87545ee736c6b0dbba6
mayabyte <mayahayden@protonmail.com>
2025-11-14 12:45:18 +0000

    fix warping after same seed retry using same seed

--- src/p2gz/warp.cpp
+++ src/p2gz/warp.cpp
@@ -249,7 +249,6 @@ void Warp::do_warp()
 
 	if (preset) {
 		preset->apply();
-		preset_status                      = PS_Stale;
 		p2gz->preset_mgr->last_used_preset = preset;
 		needs_post_load_action             = true;
 	}
@@ -484,5 +483,7 @@ void Warp::do_post_warp()
 	needs_post_load_action = false;
 	if (preset) {
 		preset->apply_post_load();
+		preset_status     = PS_Stale;
+		dest.use_set_seed = false;
 	}
 }
