p2gz/p2gz

commit 9b2850d44e9b695cffefb8a779495d90a90f62db

Enable quick-retry for AG segments (#197)
AG segments can now be retried just like cave segments. There's only one
basic retry option as same-seed and from-beginning options don't make
sense here.

Additional changes:
- Properly free old `Segment`s when the ringbuffer reaches capacity
- Use a proper type for cutscene bitflags in presets instead of two u32s
- Populate upgrades and cutscene flags in generated presets using actual
current game state rather than the previous preset
- Begin `Segment`s in the Load state to more closely match RTA segments
and to ensure one is made for AGs as well

include/p2gz/SegmentHistory.h
@@ -26,18 +26,18 @@ public:
 	void draw_2d();
 	void update();
 
-	void start_segment(u32 seed);
-	const Segment* cur_segment();
-	Segment* cur_segment_mut() { return const_cast<Segment*>(cur_segment()); }
+	Segment* start_segment();
+	Segment* cur_segment();
+	void record_squad();
 
 	bool started_creating_map;
-	bool entering_next_sublevel;
+	bool entering_next_segment;
 
 private:
 	void draw_cur_seed();
 	void draw_reset_controls(bool draw_cave_retry);
 
-	RingBuffer<32, const Segment*> segments;
+	RingBuffer<32, Segment*> segments;
 };
 
 }; // namespace gz

src/p2gz/segmentHistory.cpp
@@ -31,12 +31,12 @@ void SegmentHistory::draw_2d()
 		draw_cur_seed();
 	}
 
-	if (entering_next_sublevel || (cur_segment() && p2gz->menu->is_root_open() && in_cave_play())) {
-		draw_reset_controls(entering_next_sublevel);
+	if (entering_next_segment || (cur_segment() && p2gz->menu->is_root_open())) {
+		draw_reset_controls(entering_next_segment);
 	}
 }
 
-const Segment* SegmentHistory::cur_segment()
+Segment* SegmentHistory::cur_segment()
 {
 	if (segments.len() > 0) {
 		return segments.peek();
@@ -54,7 +54,7 @@ void SegmentHistory::update()
 	WarpDestination current_dest = current_segment->dest;
 	const u32 btn                = p2gz->controller->getButtonDown();
 
-	if (entering_next_sublevel || (p2gz->menu->is_root_open() && in_cave_play())) {
+	if (entering_next_segment || p2gz->menu->is_root_open()) {
 		// Retry same sublevel, random seed
 		if (btn & Controller::PRESS_X) {
 			current_dest.use_set_seed = false;
@@ -63,24 +63,24 @@ void SegmentHistory::update()
 			p2gz->warp->set_preset(current_segment->preset, PS_Generated);
 			p2gz->warp->do_warp();
 
-			entering_next_sublevel = false;
+			entering_next_segment = false;
 			return;
 		}
 
 		// Retry same sublevel, same seed
-		if (btn & Controller::PRESS_Y) {
+		if (btn & Controller::PRESS_Y && in_cave_play()) {
 			current_dest.use_set_seed = true;
 
 			p2gz->warp->set_dest(current_dest);
 			p2gz->warp->set_preset(current_segment->preset, PS_Generated);
 			p2gz->warp->do_warp();
 
-			entering_next_sublevel = false;
+			entering_next_segment = false;
 			return;
 		}
 	}
 
-	if (entering_next_sublevel) {
+	if (entering_next_segment && in_cave_play()) {
 		// Restart cave from the beginning
 		if (btn & Controller::PRESS_L) {
 			if (segments.len() == 0) {
@@ -127,7 +127,7 @@ void SegmentHistory::update()
 			}
 
 			p2gz->warp->do_warp();
-			entering_next_sublevel = false;
+			entering_next_segment = false;
 			return;
 		}
 	}
@@ -181,25 +181,44 @@ void SegmentHistory::draw_reset_controls(bool draw_cave_retry)
 	J2DPrint j2d         = init_j2d(glyph_size);
 
 	f32 z = 280.0f;
-	draw_ctrl(j2d, glyph_size, z, "x_btn", "replay sublevel");
-	draw_ctrl(j2d, glyph_size, z, "y_btn", "replay seed");
-	if (draw_cave_retry) {
-		draw_ctrl(j2d, glyph_size, z, "l_btn", "restart cave");
+
+	if (in_cave_play()) {
+		draw_ctrl(j2d, glyph_size, z, "x_btn", "replay sublevel");
+		draw_ctrl(j2d, glyph_size, z, "y_btn", "replay seed");
+		if (draw_cave_retry) {
+			draw_ctrl(j2d, glyph_size, z, "l_btn", "restart cave");
+		}
+	} else {
+		draw_ctrl(j2d, glyph_size, z, "x_btn", "replay segment");
 	}
 }
 
-void SegmentHistory::start_segment(u32 seed)
+Segment* SegmentHistory::start_segment()
 {
 	JKRHeap* prev_heap = sys->mSysHeap->becomeCurrentHeap();
 
 	Segment* segment     = new Segment();
-	WarpDestination dest = Warp::current_dest();
-	dest.seed            = seed;
-	dest.use_set_seed    = true;
-	segment->dest        = dest;
 	segment->preset      = nullptr; // pikis are not alive when this is run. it will be set later
 
-	segments.push(const_cast<const Segment*>(segment));
+	if (segments.atCapacity()) {
+		Segment* oldestSegment = segments.getLast();
+		delete oldestSegment;
+	}
+	segments.push(segment);
 
 	prev_heap->becomeCurrentHeap();
+
+	return segment;
+}
+
+void SegmentHistory::record_squad()
+{
+	Segment* segment = cur_segment();
+	if (!segment || !segment->preset || segment->preset->category != Generated) {
+		return;
+	}
+
+	segment->preset->squad.clear();
+	segment->preset->onion_pikis.clear();
+	PresetMgr::fill_current_pikis(segment->preset);
 }

src/p2gz/warp.cpp
@@ -60,6 +60,7 @@ Warp::Warp()
 	warping_from_menu          = false;
 	needs_post_load_action     = false;
 	warping                    = false;
+	already_saved_generators   = false;
 	preset_status              = PS_Stale;
 	cave                       = nullptr;
 	lockout_frames             = 0;
@@ -107,21 +108,6 @@ void Warp::set_preset(Preset* preset_, int preset_status_)
 
 	update_day_opt();
 	update_enter_type_opt();
-}
-
-WarpDestination Warp::current_dest()
-{
-	Game::SingleGameSection* game = static_cast<Game::SingleGameSection*>(Game::gameSystem->mSection);
-	ID32 cave_id(game->getCaveID());
-	WarpDestination dest;
-
-	dest.area         = game->mCurrentCourseInfo->mCourseIndex;
-	dest.cave         = game->mCurrentCourseInfo->getCaveIndex_FromID(cave_id) + 1;
-	dest.sublevel     = game->mCurrentFloor;
-	dest.day          = Game::gameSystem->mTimeMgr->mDayCount;
-	dest.use_set_seed = false;
-
-	return dest;
 }
 
 void Warp::set_dest(WarpDestination new_dest)
@@ -249,7 +235,6 @@ void Warp::do_warp()
 
 	if (preset) {
 		preset->apply();
-		p2gz->preset_mgr->last_used_preset = preset;
 		needs_post_load_action             = true;
 	}
 
@@ -281,6 +266,8 @@ void Warp::do_warp()
 		skip_save_prompts_opt->set_selection(true);
 		p2gz->skip_save->toggle_save_skip(true);
 	}
+
+	already_saved_generators = false;
 }
 
 void Warp::reset_cave_treasure_collections(Game::SingleGameSection* game)
@@ -350,7 +337,7 @@ void Warp::warp_to_cave(Game::SingleGameSection* game)
 	Game::playData->mCaveSaveData.mCurrentCaveID = caveID;
 
 	// Save changes to world state if we're above-ground currently
-	if (in_above_ground_play()) {
+	if (in_above_ground_play() && !already_saved_generators) {
 		game->saveToGeneratorCache(game->mCurrentCourseInfo);
 	}
 
@@ -404,7 +391,7 @@ void Warp::warp_to_area(Game::SingleGameSection* game)
 	}
 
 	// Save changes to world state if we're above-ground currently
-	if (in_above_ground_play()) {
+	if (in_above_ground_play() && !already_saved_generators) {
 		game->saveToGeneratorCache(game->mCurrentCourseInfo);
 	}
 

src/plugProjectKandoU/singleGS_Load.cpp
@@ -102,6 +102,37 @@ void LoadState::init(SingleGameSection* game, StateArg* arg)
 		break;
 	}
 
+	// @P2GZ - segment history
+	// Start new segment on any loading screen
+	gz::Segment* segment = p2gz->segment_history->start_segment();
+	gz::Preset* preset   = p2gz->warp->get_preset();
+	if (p2gz->warp->warping && preset) {
+		segment->preset = preset;
+	} else {
+		segment->preset      = p2gz->preset_mgr->create();
+		segment->preset->day = Game::gameSystem->mTimeMgr->mDayCount;
+	}
+
+	gz::WarpDestination dest;
+	dest.area         = game->mCurrentCourseInfo->mCourseIndex;
+	dest.day          = Game::gameSystem->mTimeMgr->mDayCount;
+	dest.use_set_seed = false;
+
+	if (!(mIsCaveLoad || mIsCaveDeeper)) {
+		dest.cave = 0;
+		if (mGameLoadType == Game::SingleGame::MapEnter_NewDay) {
+			segment->preset->enter_kind = gz::PEK_FromMap;
+		} else {
+			segment->preset->enter_kind = gz::PEK_FromCave;
+		}
+	} else {
+		ID32 cave_id(game->getCaveID());
+		dest.cave     = game->mCurrentCourseInfo->getCaveIndex_FromID(cave_id) + 1;
+		dest.sublevel = game->mCurrentFloor;
+	}
+
+	segment->dest = dest;
+
 	// @P2GZ localization-swap:
 	// update treasure region before pellet loading if required
 	if (p2gz->localization_op->require_update()) {

include/p2gz/CutsceneToggle.h
@@ -99,6 +99,42 @@ private:
 	CutsceneMenuOption* option;
 };
 
+struct CutscenesBitfield {
+public:
+	CutscenesBitfield()
+	{
+		bf1 = 0;
+		bf2 = 0;
+	}
+	CutscenesBitfield(CutscenesBitfield& other)
+	{
+		bf1 = other.bf1;
+		bf2 = other.bf2;
+	}
+
+	bool cutscene_played(Game::DemoFlags flag)
+	{
+		if (flag < 32) {
+			return bf1 & (1 << flag);
+		} else {
+			return bf2 & (1 << (flag - 32));
+		}
+	}
+
+	void set_cutscene_played(Game::DemoFlags flag)
+	{
+		if (flag < 32) {
+			bf1 |= (1 << flag);
+		} else {
+			bf2 |= (1 << (flag - 32));
+		}
+	}
+
+private:
+	u32 bf1;
+	u32 bf2;
+};
+
 struct CutsceneMgr {
 public:
 	CutsceneMgr();
@@ -107,6 +143,7 @@ public:
 	void update();
 
 	CutsceneToggle* get_toggle(Game::DemoFlags id);
+	CutscenesBitfield get_cur_cutscenes();
 	void reset_all();
 
 	static CutsceneMenuOption* create_option(Game::DemoFlags id);

include/p2gz/EKEditor.h
@@ -1,6 +1,7 @@
 #ifndef _GZ_EK_EDITOR_H
 #define _GZ_EK_EDITOR_H
 
+#include <BitFlag.h>
 #include <p2gz/gzmenu.h>
 #include <Game/gamePlayData.h>
 
@@ -15,6 +16,7 @@ public:
 	void set_upgrade(Game::OlimarData::ItemIndex, bool enabled);
 	void reset_all();
 	void check_upgrades();
+	BitFlag<u16> get_upgrades_bitfield();
 
 private:
 	void set_menu_opt(Game::OlimarData::ItemIndex, const char* opt_name);

include/p2gz/Preset.h
@@ -1,10 +1,12 @@
 #ifndef _GZ_PRESET_H
 #define _GZ_PRESET_H
 
+#include <BitFlag.h>
 #include <IDelegate.h>
 #include <p2gz/gzCollections.h>
 #include <p2gz/gzmenu.h>
 #include <p2gz/warp.h>
+#include <p2gz/CutsceneToggle.h>
 #include <types.h>
 #include <Game/PikiContainer.h>
 #include <JSystem/J2D/J2DPrint.h>
@@ -90,12 +92,11 @@ public:
 	Game::PikiContainer onion_pikis;
 	bool bitters_unlocked;
 	bool spicies_unlocked;
-	u16 upgrades; // bitflags
+	BitFlag<u16> upgrades;
 	u8 num_bitters;
 	u8 num_spicies;
 	f32 time;
-	u32 cutscene_flags1; // bitflags
-	u32 cutscene_flags2; // bitflags
+	CutscenesBitfield cutscenes;
 	Vec<const char*> destroyed_gates;
 	Vec<const char*> finished_bridges;
 	Vec<const char*> bags_flattened;
@@ -113,12 +114,12 @@ public:
 	PresetMgr();
 
 	Preset* create();
+	static void fill_current_pikis(Preset* preset);
 
 	Preset* suggested_preset(WarpDestination dest, PresetCategory category);
 	Preset* find(const char* name, PresetCategory category);
 
 	Vec<Preset*> presets;
-	Preset* last_used_preset;
 };
 
 struct PresetMenuOption : public MenuOption {

include/p2gz/gzCollections.h
@@ -43,6 +43,15 @@ struct RingBuffer {
 		return mBuf[(mBufHead + N - n - 1) % N];
 	}
 
+	bool atCapacity() { return mLen == N; }
+
+	/// Gets the oldest thing in the ring buffer. Only save to call if `atCapacity()` returns true.
+	T getLast()
+	{
+		GZASSERTLINE(mLen == N);
+		return peekN(N);
+	}
+
 private:
 	size_t mLen;
 	size_t mBufHead;

commit a85fe3818c1c4d0a731eac4cba82d44ae3e80365

Fix "over-completing" caves (#223)
- Resets cave crop data when warping or retrying to avoid getting cave
completions at incorrect times
- Fixes a bug introduced in https://github.com/p2gz/p2gz/pull/197
(probably) where sublevel retry would bring you to the previous floor
sometimes

There's some overlap in this PR with
https://github.com/p2gz/p2gz/pull/222 for test code. I'll sort it out in
the rebase when one of them gets merged.

include/p2gz/SegmentHistory.h
@@ -30,6 +30,10 @@ public:
 	Segment* cur_segment();
 	void record_squad();
 
+	void retry_segment();
+	void retry_same_seed();
+	void retry_cave();
+
 	bool started_creating_map;
 	bool entering_next_segment;
 

include/p2gz/Test.h
@@ -14,12 +14,13 @@ namespace test {
 #define TEST(name, ...) ((new Test(name))__VA_ARGS__ WAIT(30))
 #define __TEST_OP(op)   ->push(op)
 
-#define __COMPOUND(...)        (new CompoundOp()) __VA_ARGS__
-#define PRESS(btn)             __TEST_OP(new ButtonInput(btn)) __TEST_OP(new Wait(1))
-#define DBL_DPAD_L             PRESS(PAD_BUTTON_LEFT) PRESS(PAD_BUTTON_LEFT)
-#define MASH_TEXT              PRESS(PAD_BUTTON_A) PRESS(PAD_BUTTON_B)
-#define SKIP_CUTSCENE          __TEST_OP(new ActionOp(new FreeDelegate(&skip_movie)))
-#define SKIP_LOAD_CUTSCENES    DO_UNTIL(SKIP_CUTSCENE, in_gameplay)
+#define __COMPOUND(...)     (new CompoundOp()) __VA_ARGS__
+#define PRESS(btn)          __TEST_OP(new ButtonInput(btn)) __TEST_OP(new Wait(1))
+#define DBL_DPAD_L          PRESS(PAD_BUTTON_LEFT) PRESS(PAD_BUTTON_LEFT)
+#define MASH_TEXT           PRESS(PAD_BUTTON_A) PRESS(PAD_BUTTON_B)
+#define SKIP_CUTSCENE       __TEST_OP(new ActionOp(new FreeDelegate(&skip_movie)))
+#define SKIP_LOAD_CUTSCENES DO_UNTIL(SKIP_CUTSCENE, in_gameplay)
+#define RETRY_SEGMENT       __TEST_OP(new ActionOp(new Delegate<SegmentHistory>(p2gz->segment_history, &SegmentHistory::retry_segment)))
 
 #define DO_ACTION(delegate)    __TEST_OP(new ActionOp(delegate))
 #define DO_N(num, ...)         __TEST_OP(new DoN(__COMPOUND(__VA_ARGS__), num))

src/p2gz/segmentHistory.cpp
@@ -44,38 +44,107 @@ Segment* SegmentHistory::cur_segment()
 	return nullptr;
 }
 
-void SegmentHistory::update()
+void SegmentHistory::retry_segment()
 {
 	const Segment* current_segment = cur_segment();
 	if (!current_segment) {
 		return;
 	}
-
 	WarpDestination current_dest = current_segment->dest;
-	const u32 btn                = p2gz->controller->getButtonDown();
 
+	current_dest.use_set_seed = false;
+	p2gz->warp->set_dest(current_dest);
+	p2gz->warp->set_preset(current_segment->preset, PS_Generated);
+	p2gz->warp->do_warp();
+
+	entering_next_segment = false;
+}
+
+void SegmentHistory::retry_same_seed()
+{
+	const Segment* current_segment = cur_segment();
+	if (!current_segment) {
+		return;
+	}
+	WarpDestination current_dest = current_segment->dest;
+
+	current_dest.use_set_seed = true;
+
+	p2gz->warp->set_dest(current_dest);
+	p2gz->warp->set_preset(current_segment->preset, PS_Generated);
+	p2gz->warp->do_warp();
+
+	entering_next_segment = false;
+}
+
+void SegmentHistory::retry_cave()
+{
+	const Segment* current_segment = cur_segment();
+	if (!current_segment) {
+		return;
+	}
+	WarpDestination current_dest = current_segment->dest;
+
+	if (segments.len() == 0) {
+		return;
+	}
+
+	// Find segment for the first floor of the cave
+	const Segment* floor0_segment = nullptr;
+	for (size_t i = 0; i < segments.len(); i++) {
+		const Segment* this_segment = segments.peekN(i);
+		if (!this_segment) {
+			break;
+		}
+
+		if (this_segment->dest.cave == current_dest.cave) {
+			if (this_segment->dest.area == current_dest.area && this_segment->dest.sublevel == 0) {
+				floor0_segment = this_segment;
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+
+	if (!floor0_segment) {
+		floor0_segment = current_segment;
+	}
+
+	WarpDestination floor0_dest = floor0_segment->dest;
+	floor0_dest.use_set_seed    = false;
+	if (floor0_dest.sublevel != 0) {
+		floor0_dest.sublevel = 0;
+		// If we don't find history for floor 0 in this cave, get the recommended preset for it.
+		// TODO: currently assumes the PoD preset. Adjust to reflect AT in the future
+		PresetCategory cat = PoD;
+		if (floor0_segment->preset && floor0_segment->preset->category != Generated) {
+			cat = floor0_segment->preset->category;
+		}
+		p2gz->warp->set_dest(floor0_dest);
+		p2gz->warp->set_preset(p2gz->preset_mgr->suggested_preset(floor0_dest, cat), PS_Suggested);
+	} else {
+		p2gz->warp->set_dest(floor0_dest);
+		p2gz->warp->set_preset(floor0_segment->preset, PS_Generated);
+	}
+
+	p2gz->warp->do_warp();
+	entering_next_segment = false;
+}
+
+void SegmentHistory::update()
+{
+	const u32 btn = p2gz->controller->getButtonDown();
 	if (entering_next_segment || p2gz->menu->is_root_open()) {
 		// Retry same sublevel, random seed
 		if (btn & Controller::PRESS_X) {
-			current_dest.use_set_seed = false;
-
-			p2gz->warp->set_dest(current_dest);
-			p2gz->warp->set_preset(current_segment->preset, PS_Generated);
-			p2gz->warp->do_warp();
-
-			entering_next_segment = false;
+			retry_segment();
 			return;
 		}
 
 		// Retry same sublevel, same seed
 		if (btn & Controller::PRESS_Y && in_cave_play()) {
-			current_dest.use_set_seed = true;
-
-			p2gz->warp->set_dest(current_dest);
-			p2gz->warp->set_preset(current_segment->preset, PS_Generated);
-			p2gz->warp->do_warp();
-
-			entering_next_segment = false;
+			retry_same_seed();
 			return;
 		}
 	}
@@ -83,51 +152,7 @@ void SegmentHistory::update()
 	if (entering_next_segment && in_cave_play()) {
 		// Restart cave from the beginning
 		if (btn & Controller::PRESS_L) {
-			if (segments.len() == 0) {
-				return;
-			}
-
-			// Find segment for the first floor of the cave
-			const Segment* floor0_segment = nullptr;
-			for (size_t i = 0; i < segments.len(); i++) {
-				const Segment* this_segment = segments.peekN(i);
-				if (!this_segment) {
-					break;
-				}
-
-				if (this_segment->dest.cave == current_dest.cave) {
-					if (this_segment->dest.area == current_dest.area && this_segment->dest.sublevel == 0) {
-						floor0_segment = this_segment;
-						break;
-					}
-				} else {
-					break;
-				}
-			}
-
-			if (!floor0_segment) {
-				floor0_segment = current_segment;
-			}
-
-			WarpDestination floor0_dest = floor0_segment->dest;
-			floor0_dest.use_set_seed    = false;
-			if (floor0_dest.sublevel != 0) {
-				floor0_dest.sublevel = 0;
-				// If we don't find history for floor 0 in this cave, get the recommended preset for it.
-				// TODO: currently assumes the PoD preset. Adjust to reflect AT in the future
-				PresetCategory cat = PoD;
-				if (floor0_segment->preset && floor0_segment->preset->category != Generated) {
-					cat = floor0_segment->preset->category;
-				}
-				p2gz->warp->set_dest(floor0_dest);
-				p2gz->warp->set_preset(p2gz->preset_mgr->suggested_preset(floor0_dest, cat), PS_Suggested);
-			} else {
-				p2gz->warp->set_dest(floor0_dest);
-				p2gz->warp->set_preset(floor0_segment->preset, PS_Generated);
-			}
-
-			p2gz->warp->do_warp();
-			entering_next_segment = false;
+			retry_cave();
 			return;
 		}
 	}

src/p2gz/warp.cpp
@@ -306,14 +306,16 @@ void Warp::reset_cave_treasure_collections(Game::SingleGameSection* game)
 
 	for (int i = 0; i < counter_otakara.getNumKinds(); i++) {
 		Game::playData->losePellet(pelmgr, i);
-		counter_otakara(i) = 0;
+		Game::playData->mCaveCropMemory->mOtakara(i) = 0;
+		counter_otakara(i)                           = 0;
 	}
 
 	pelmgr                          = Game::PelletItem::mgr;
 	Game::KindCounter& counter_item = mem->mItem;
 	for (int i = 0; i < counter_item.getNumKinds(); i++) {
 		Game::playData->losePellet(pelmgr, i);
-		counter_item(i) = 0;
+		Game::playData->mCaveCropMemory->mItem(i) = 0;
+		counter_item(i)                           = 0;
 	}
 }
 
@@ -374,6 +376,7 @@ void Warp::warp_to_cave(Game::SingleGameSection* game)
 	game->mCaveID                               = caveID;
 	game->mCaveIndex                            = caveID.getID();
 	game->mCurrentFloor                         = dest.sublevel;
+	Game::playData->mCaveSaveData.mCurrentFloor = dest.sublevel;
 	Game::playData->mCaveSaveData.mActiveNaviID = active_captain;
 	strcpy(game->mCaveFilename, cave->mCaveFilename);
 

src/plugProjectKandoU/singleGS_Load.cpp
@@ -128,7 +128,7 @@ void LoadState::init(SingleGameSection* game, StateArg* arg)
 	} else {
 		ID32 cave_id(game->getCaveID());
 		dest.cave     = game->mCurrentCourseInfo->getCaveIndex_FromID(cave_id) + 1;
-		dest.sublevel = game->mCurrentFloor;
+		dest.sublevel = Game::playData->mCaveSaveData.mCurrentFloor;
 	}
 
 	segment->dest = dest;

src/p2gz/test/create_all_tests.cpp
@@ -40,6 +40,23 @@ void expect_captain_hp(f32 expected)
 	GZEXPECT(absF(actual - expected) < 0.1f, "Captain health is incorrect");
 }
 
+#define ASSERT_LOCATION(area, cave, sublevel) (new FreeBoundDelegate3<int, int, int>(&assert_location, area, cave, sublevel))
+void assert_location(int area, int cave, int sublevel)
+{
+	Game::SingleGameSection* game = static_cast<Game::SingleGameSection*>(Game::gameSystem->mSection);
+	OSReport("current area = %d\n", game->mCurrentCourseInfo->mCourseIndex);
+	GZASSERTLINE(game->mCurrentCourseInfo->mCourseIndex == area);
+
+	Game::CourseInfo* dst_course_info = Game::stageList->getCourseInfo(area);
+	ID32 caveID(dst_course_info->getCaveID_FromIndex(cave - 1));
+
+	OSReport("current cave ID = %d\n", game->mCaveID.getID());
+	GZASSERTLINE(game->mCaveID.getID() == caveID.getID());
+
+	OSReport("current floor = %d\n", game->mCurrentFloor);
+	GZASSERTLINE(game->mCurrentFloor == sublevel - 1);
+}
+
 void assert_has_cos_upgrades()
 {
 	GZASSERTLINE(Game::playData->mOlimarData->hasItem(Game::OlimarData::ODII_SphericalAtlas));
@@ -61,6 +78,15 @@ void TestRunner::create_all_tests()
         PRESS(PAD_BUTTON_UP)
         PRESS(PAD_BUTTON_A)
         SKIP_LOAD_CUTSCENES
+    ));
+    tests.push(TEST("sublevel retry works as expected",
+        DO_ACTION(WARP_TO_DEST(1, 1, 1))
+        SKIP_LOAD_CUTSCENES
+        DO_ACTION(WARP_TO_DEST(1, 1, 2))
+        SKIP_LOAD_CUTSCENES
+        RETRY_SEGMENT
+        SKIP_LOAD_CUTSCENES
+        DO_ACTION(ASSERT_LOCATION(1, 1, 2))
     ));
 	tests.push(TEST("warp to ww",
         DBL_DPAD_L