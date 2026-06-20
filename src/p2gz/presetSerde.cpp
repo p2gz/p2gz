04c0758febee0dd940a6cbe38cf9a3b01ab20552
Jpep <jean.pi.abajour@gmail.com>
2026-06-20 10:01:10 -0400

    Delete unclear comment.

--- src/p2gz/presetSerde.cpp
+++ src/p2gz/presetSerde.cpp
@@ -288,7 +288,7 @@ void Preset::read(Stream& input)
 	new_area_zoom.clear();
 	new_area_zoom.typeView = static_cast<u16>(input.readInt());
 	OSReport("Right before reading play_repay_demo \n");
-	play_repay_demo = input.readInt() > 0; // read % cutscene on the next line from pokos
+	play_repay_demo = input.readInt() > 0; 
 }
 
 void Preset::write(Stream& output)
